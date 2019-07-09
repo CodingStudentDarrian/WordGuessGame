21bc1c053c9f320056e93799f00d11765e381b1c
Darrian <50306558+CodingStudentDarrian@users.noreply.github.com>
2019-07-09 14:27:18 -0500

    add files
    fix hw

---
+++ assets/javaScript/game.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+
+    var possibleWords = ["Krillin", "Vegeta", "Goku", "Master Roshi", "Tien", "Jiren", "Beerus",
+        "Whis", "Piccolo", "Gohan", " Mr Satan", "Bulma", "Chi Chi", "Chiaotzu",
+        "Trunks", "", "kefla", "Kale", "Android 18", "Frieza",
+        "Zeno", "Caulifla", "Android 17", "Future Trunks", "Toppo"
+    ]
+
+    const maxGuess = 10
+    var pauseGame = false
+
+    var guessedLetters = []
+    var guessingWord = []
+    var wordToMatch
+    var numGuess
+    var wins = 0
+
+    resetGame()
+
+
+    document.onkeypress = function(event) {
+        if (isAlpha(event.key) && !pauseGame) {
+            checkForLetter(event.key.toUpperCase())
+        }
+    }
+
+    function checkForLetter(letter) {
+        var foundLetter = false
+        var correctSound = document.createElement("audio")
+        var incorrectSound = document.createElement("audio")
+        correctSound.setAttribute("src", "")
+        incorrectSound.setAttribute("src", "")
+
+
+        for (var i = 0, j = wordToMatch.length; i < j; i++) {
+            if (letter === wordToMatch[i]) {
+                guessingWord[i] = letter
+                foundLetter = true
+                correctSound.play()
+
+                if (guessingWord.join("") === wordToMatch) {
+
+                    wins++
+                    pauseGame = true
+                    updateDisplay()
+                    setTimeout(resetGame, 5000)
+                }
+            }
+        }
+
+        if (!foundLetter) {
+            incorrectSound.play()
+
+            if (!guessedLetters.includes(letter)) {
+
+                guessedLetters.push(letter)
+
+                numGuess--
+            }
+            if (numGuess === 0) {
+
+                guessingWord = wordToMatch.split()
+                pauseGame = true
+                setTimeout(resetGame, 5000)
+            }
+        }
+
+        updateDisplay()
+
+    }
+
+    function isAlpha(ch) {
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    function resetGame() {
+        numGuess = maxGuess
+        pauseGame = false
+
+
+        wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase()
+        console.log(wordToMatch)
+
+
+        guessedLetters = []
+        guessingWord = []
+
+
+        for (var i = 0, j = wordToMatch.length; i < j; i++) {
+
+            if (wordToMatch[i] === " ") {
+                guessingWord.push(" ")
+            } else {
+                guessingWord.push("_")
+            }
+        }
+
+
+        updateDisplay()
+    }
+
+    function updateDisplay() {
+        document.getElementById("totalWins").innerText = wins
+        document.getElementById("currentWord").innerText = guessingWord.join("")
+        document.getElementById("remainingGuesses").innerText = numGuess
+        document.getElementById("guessedLetters").innerText = guessedLetters.join(" ")
+    }
+})
